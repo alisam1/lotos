2c81b7eb169026e8fd30d95506f4a23644a69e3d
alisam1 <alisa-m2@yandex.ru>
2019-08-22 18:01:32 +0300

    New

---
+++ source/js/main.js
@@ -0,0 +1,8 @@
+$('#fullPage').fullpage();
+
+$('#fullPage').fullpage({
+    sectionSelector: '.vertical-scrolling',
+    slideSelector: '.horizontal-scrolling',
+    controlArrows: false
+    // more options here
+});
